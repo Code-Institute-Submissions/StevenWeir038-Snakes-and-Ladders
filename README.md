7a59680b95f80caa6695ce4e83cc40d2accd16b7
StevenWeir038 <weir_steven@btinternet.com>
2021-11-13 16:41:18 +0000

    Modify game rules for added challenge

--- README.md
+++ README.md
@@ -4,24 +4,24 @@
 
 ## Game Rules
 **Game Requirements**
-Number of players: 2-4
 
-Required: 1 dice (six sided), pawns (1 for each player), playing board
+- Number of players: 2-4
+- Required: 1 dice (six sided), pawns (1 for each player), playing board
 
 **Pre-game**
 - Each player takes a different color pawn and throws the dice.
 
 **Game**
 - The first player throws the dice and moves their pawn according to the number shown on the dice.
-- Each time a player throws a 6, they are entitled to roll the dice and move again.
-- If a player's pawn ends its move at the foot of a ladder, the pawn must move immediatelly to the square at the top of that ladder.
-- If a player's pawn ends it move at the head of a snake, the pawn must immediately move to the tail of that snake.
 - If a player's pawn lands on an empty square there is no effect.
-- If a player's pawn lands on a square occupied by an opponents pawn, that pawn is removed from the board and they must start again.
-- An exact throw is required to reach square 100.
+- If a player's pawn ends its move at the foot of a ladder, the pawn must move immediately to the square at the top of that ladder.
+- If a player's pawn ends it move at the head of a snake, the pawn must immediately move to the tail of that snake.
+- Each time a player throws a 6, they are entitled to roll the dice and move again.
+- (Additional rule) If a player's pawn lands on a square occupied by an opponents pawn, that pawn is removed from the board and they must start again. 
+- (Additional Rule) An exact throw is required to reach square 100.  If the throw exceeds 100 the player must move backwards. Watch out for the snakes!
 
 **Victory Condition**
-Be the first player to reach square *100*. 
+- Be the first player to reach square *100*. 
 
 ## Game Mapping
 The [game logic](docs/wireframes) diagram was created using [Lucidchart](https://www.lucidchart.com/pages/).
--- run.py
+++ run.py
@@ -7,31 +7,38 @@ def game_instructions():
     # and/or using Implicit concatenation to keep strings < 80 chars long
     # https://stackoverflow.com/questions/1874592/how-to-write-very-long-string-that-conforms-with-pep8-and-prevent-e501
     game_about = """
-    Game Requirements
+    GAME REQUIREMENTS
     - Number of players: 2-4
     - Required: 1 dice (six sided), pawns (1 for each player), playing board
 
-    Pre-game
+    PRE-GAME
     - Each player takes a different color pawn and throws the dice.
 
-    Game
+    GAME RULES
     - The first player throws the dice and moves their pawn according to the
       number shown on the dice.
-    - Each time a player throws a 6, they are entitled to roll the dice and
-      move again.
+    - If a player's pawn lands on an empty square there is no effect.
     - If a player's pawn ends its move at the foot of a ladder, the pawn must
       move immediately to the square at the top of that ladder.
     - If a player's pawn ends it move at the head of a snake, the pawn must
       immediately move to the tail of that snake.
-    - If a player's pawn lands on an empty square there is no effect.
-    - If a player's pawn lands on a square occupied by an opponents pawn, that
-      pawn is removed from the board and they must start again.
-    - An exact throw is required to reach square 100.
+    - Each time a player throws a 6, they are entitled to roll the dice and
+      move again.
+    - (Additional rule) If a player's pawn lands on a square occupied by an 
+      opponents pawn, that pawn is removed from the board and they must start 
+      again. 
+    - (Additional Rule) An exact throw is required to reach square 100.  If
+      the throw exceeds 100 the player must move backwards.
+      Watch out for the snakes!
 
-    Victory Condition
+    VICTORY CONDITIONS
     - Be the first player to reach square *100*.
     """
     print(game_about)
+
+
+def game_setup():
+
 
 
 def main():
@@ -39,6 +46,6 @@ def main():
     Run all program functions
     """
     game_instructions()
-
+    game_setup()
 
 main()
