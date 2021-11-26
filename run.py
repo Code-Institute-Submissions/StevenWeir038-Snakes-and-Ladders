ab415208816e7131cf0d0c8b1c9f555f87cfd18c
StevenWeir038 <weir_steven@btinternet.com>
2021-11-26 10:49:35 +0000

    Employ multiline f-strings for pep8 compliance

--- run.py
+++ run.py
@@ -103,7 +103,8 @@ def game_setup():
     while True:
 
         # Immediately convert string input from user to an integer
-        player_count = int(input("Enter number of players between 2 and 4:\n"))
+        player_count = int(input(
+            "Enter number of players between 2 and 4:\n"))
 
         if validate_player_count(player_count):
             print("\nValid input. Creating players...\n")
@@ -161,16 +162,16 @@ def validate_player_count(player_count):
     return True
 
 
-def roll_dice(player_instance):
+def roll_dice(player_inst):
     roll = random.randint(1, 6)
     # ternary expression to evaluate True or False
     another_turn = True if roll == 6 else False
-    # assign bool value of another_turn variable to player_instance attribute
-    player_instance.extra_roll = another_turn
+    # assign bool value of another_turn variable to player_inst attribute
+    player_inst.extra_roll = another_turn
     return roll
 
 
-def turn(player_id, player_instance, curr_position):
+def turn(player_id, player_inst, curr_position):
     """
     For each player turn:
     1. Simulate dice roll - move to roll_dice()
@@ -182,10 +183,12 @@ def turn(player_id, player_instance, curr_position):
     if so give them another roll - move to snl_game()
     6. By default, move to next player.
     """
-    roll_num = roll_dice(player_instance)
+    roll_num = roll_dice(player_inst)
     new_position = curr_position + roll_num
     print(
-        f"Player '{player_id}' rolled a '{roll_num}' and moves from square '{curr_position}' to square '{new_position}'.")
+        f"""Player '{player_id}' rolled a '{roll_num}' and moves from square '
+        {curr_position}' to square '{new_position}'.""")
+    
     # evaluate if pawn has landed on a special square.
     # If so player moves from key to value in Snake/Ladder dict,
     # reassign value for current player object instance curr_position attribute
@@ -193,15 +196,17 @@ def turn(player_id, player_instance, curr_position):
     # snake tail which equals the SNAKE_HEAD value
     if new_position in SNAKE_HEAD:
         new_position = SNAKE_HEAD[new_position]
-        print(f"Player '{player_id}' landed on a SNAKE_HEAD and moves to square '{new_position}'.")
+        print(f"""Player '{player_id}' landed on a SNAKE_HEAD and moves to
+         square '{new_position}'.""")
     elif new_position in LADDER_FOOT:
         new_position = LADDER_FOOT[new_position]
-        print(f"Player '{player_id}' landed on a LADDER_FOOT and moves to square '{new_position}'.")
+        print(f"""Player '{player_id}' landed on a LADDER_FOOT and moves to
+         square '{new_position}'.""")
     return new_position
 
 
-def check_win(player_id, player_instance):
-    if player_instance.curr_square >= 100:
+def check_win(player_id, player_inst):
+    if player_inst.curr_square >= 100:
         print(f"Player '{player_id}' wins!\n")
         return True
 
@@ -211,40 +216,46 @@ def snl_game(players):
     Iterate players, loop through each until win condition met
     """
     # infinite loop needed to keep game live until victory condition met
-    while True:
     # for i in range(50):  # testing for 50 turns
+    while True:
 
-        for player_id, player_instance in players.items():
+        for player_id, player_inst in players.items():
             # key is the player iterable, value is the Player object instance
             # establish current player's location on board and
             # assign the object attribute to 'curr_position' using .notation
-            curr_position = player_instance.curr_square
-            print(f"Player '{player_id}' current location is square '{curr_position}'.")  # testing
+            curr_position = player_inst.curr_square
+            print(f"""Player '{player_id}' current location is
+             square '{curr_position}'.""")  # testing
             # now pass curr_position variable to turn() function to process
             # the players new location based off their next dice roll
-            new_position = turn(player_id, player_instance, curr_position)
-            print(f"player '{player_id}' rolled a six value is '{player_instance.extra_roll}'.")
+            new_position = turn(player_id, player_inst, curr_position)
+            print(f"""player '{player_id}' rolled a six value is
+             '{player_inst.extra_roll}'.""")
             # update player instance attribute with returned value from turn()
-            player_instance.curr_square = new_position  # testing
-            print(f"Player '{player_id}' new location is square '{player_instance.curr_square}'.\n")  # testing
+            player_inst.curr_square = new_position  # testing
+            print(f"""Player '{player_id}' new location is square '{
+                player_inst.curr_square}'.\n""")  # testing
 
             # check if win condition met
-            winner = check_win(player_id, player_instance)
+            winner = check_win(player_id, player_inst)
             if winner:
                 exit()
             else:
                 # repeat same iteration - https://stackoverflow.com/a/7293992
                 # check if player rolled a six.  Default is False,
-                extra_roll = player_instance.extra_roll
+                extra_roll = player_inst.extra_roll
                 if extra_roll is True:
-                    curr_position = player_instance.curr_square
-                    print(f"Player '{player_id}' current location is square '{curr_position}'.")  # testing
-                    new_position = turn(player_id, player_instance, curr_position)
-                    print(f"player '{player_id}' rolled a six value is '{player_instance.extra_roll}'.")
-                    player_instance.curr_square = new_position  # testing
-                    print(f"Player '{player_id}' new location is square '{player_instance.curr_square}'.\n")  # testing
+                    curr_position = player_inst.curr_square
+                    print(f"""Player '{player_id}' current location is square
+                     '{curr_position}'.""")  # testing
+                    new_position = turn(player_id, player_inst, curr_position)
+                    print(f"""player '{player_id}' rolled a six value is
+                     '{player_inst.extra_roll}'.""")
+                    player_inst.curr_square = new_position  # testing
+                    print(f"""Player '{player_id}' new location is square
+                     '{player_inst.curr_square}'.\n""")  # testing
                     # check if win condition met
-                    winner = check_win(player_id, player_instance)
+                    winner = check_win(player_id, player_inst)
                     if winner:
                         exit()
 
@@ -260,7 +271,7 @@ def main():
     game_instructions()
     # game setup returns validated number for players input by user
     players = game_setup()  # players = dict of players rtnd from game_setup()
-    # print(f"{players}\n")  # testing - shows diff memory allocation for each instance
+    # print(f"{players}\n")  # testing - shows diff mem allocs
     snl_game(players)  # pass 'players' dictionary to the game
 
 
